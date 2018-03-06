1f24ba8406baaa51496572bc33a576695e7e64f0
Gediminas Ivanauskas <gediminasiv@Gediminass-MBP.lan>
2018-03-06 13:57:47 +0200

    Add currency output

--- sample/views.py
+++ sample/views.py
@@ -2,16 +2,30 @@ import krakenex
 from django.shortcuts import render
 from django.conf import settings
 from binance.client import Client
+from pprint import pprint # remove
 
 kraken = krakenex.API(key=settings.KRAKEN_API_KEY, secret=settings.KRAKEN_API_SECRET)
 binance = Client(settings.BINANCE_API_KEY, settings.BINANCE_API_SECRET)
 
 def index(request):
     kraken_balance_info = kraken.query_private('Balance')
-
     bitrex_balance_info = binance.get_account()
 
+    if 'result' in kraken_balance_info:
+        kraken_balance_info = kraken_balance_info['result']
+    else:
+        kraken_balance_info = {}
+
+    if 'balances' in bitrex_balance_info:
+        bitrex_balance_info = bitrex_balance_info['balances']
+    else:
+        bitrex_balance_info = {}
+
+    # For real circumstances I would call the exchanges only after loading the template page with JS
+
+    pprint(bitrex_balance_info)
+
     return render(request, 'app/index.html', {
-        'kraken_balance_info': kraken_balance_info,
+        'kraken_balance_info': kraken_balance_info.items(),
         'bitrex_balance_info': bitrex_balance_info
     })
